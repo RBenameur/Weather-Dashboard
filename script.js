29c72f67b9f74a3905488b306a32ed465173d76a
Rania Benameur <raniabenameur97@gmail.com>
2023-02-03 14:35:53 +0000

    updated text within #today element

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+var currentTime = moment().format('(DD/MM/YYYY)');
+
 var apiKey = '87b24c5018ab113f42b86c0bdc18e42a' ;
 
 var baseURL = 'https://api.openweathermap.org/data/2.5/';
 
-var weatherURL = baseURL + `weather?&appid=${apiKey}&units=metric&q=`;
+var currentURL = baseURL + `weather?&appid=${apiKey}&units=metric&`;
 
 var forecastURL = baseURL + `forecast?&appid=${apiKey}&units=metric&`;
 
@@ -12,22 +14,19 @@ var forecastURL = baseURL + `forecast?&appid=${apiKey}&units=metric&`;
 
 
 function fetchCurrentWeather(search) {
-    $.get(weatherURL + search).then(function(data) {
+    $.get(currentURL + `q=${search}`).then(function(data) {
         console.log(data);
         
         var main = data.main;
         var convertedWindSpd = data.wind.speed * 3.6;
-        var currentTime = moment().format('(DD/MM/YYYY)');
 
 
         $('#today').html(`
         <h3 class="mt-1 h3">${data.name} ${currentTime} <span>${data.weather[0].icon}</span></h3>
         <p>Temp: ${main.temp}&#8451;</p>
-        <p>${convertedWindSpd.toFixed(2)} KPH</p>
-        <p>${main.humidity} %</p>
+        <p>Wind: ${convertedWindSpd.toFixed(2)} KPH</p>
+        <p>Humidity: ${main.humidity} %</p>
         `);
-
-
     });
 };
 
